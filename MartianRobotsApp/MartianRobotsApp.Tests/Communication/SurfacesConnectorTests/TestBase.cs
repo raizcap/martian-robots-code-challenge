cd7d9926736d193eae90291b0ff902d710e0ecb8
Ramón Izquierdo <raizcap@gmail.com>
2023-03-28 19:48:41 +0200

    Tests for communication layer

---
+++ MartianRobotsApp/MartianRobotsApp.Tests/Communication/SurfacesConnectorTests/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MartianRobotsApp.Communication;
+using MartianRobotsApp.Services;
+using NSubstitute;
+
+namespace MartianRobotsApp.Tests.Communication.SurfacesConnectorTests
+{
+	[TestClass]
+	public abstract class TestBase
+	{
+        protected ISurfacesConnector TestObject;
+        protected const string BASE_URL = "http://localhost:5005/Surfaces/";
+        protected const string GET_BY_SIZE = "GetSurfaceBySize?XSize={0}&YSize={1}";
+        protected const string ADD = "AddSurface?XSize={0}&YSize={1}";
+        protected readonly IHttpClientService mHttpClient;
+
+        public TestBase()
+        {
+            mHttpClient = Substitute.For<IHttpClientService>();
+
+            OnInit();
+
+            TestObject = new SurfacesConnector(mHttpClient);
+        }
+
+        [TestInitialize]
+        public abstract void OnInit();
+    }
+}
+
