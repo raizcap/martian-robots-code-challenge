f9ebd4a6760e2204ca4376ed52bf64f57f4c1caf
raizcap <50403647+raizcap@users.noreply.github.com>
2023-03-31 15:00:14 +0200

    Setup script for winodws (#19)
    * Added Windows setup file -PowerShell script-
    
    * Added a "pause" in the end of Mac/Linux and Windows scripts
    
    * Last changes to readme file

--- MacLinuxSetup.sh
+++ MacLinuxSetup.sh
@@ -1,10 +1,15 @@
-docker network create codechallenge-network
+#docker network create codechallenge-network
 
-docker run --name CodeChallengeDB --network codechallenge-network -p 1433:1433 -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Cod3.Challeng3' -d mcr.microsoft.com/mssql/server:2022-latest
+#docker run --name CodeChallengeDB --network codechallenge-network -p 1433:1433 -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Cod3.Challeng3' -d mcr.microsoft.com/mssql/server:2022-latest
 
-docker build -t code-challenge:v1 ./API
+#docker build -t code-challenge:v1 ./API
 
-docker run --name CodeChallengeAPI --network codechallenge-network -p 5005:5005 -e 'ASPNETCORE_URLS=http://*:5005' -d code-challenge:v1
+#docker run --name CodeChallengeAPI --network codechallenge-network -p 5005:5005 -e 'ASPNETCORE_URLS=http://*:5005' -d code-challenge:v1
 
-dotnet restore ./MartianRobotsApp/MartianRobotsApp/MartianRobotsApp.csproj --disable-parallel
-dotnet build ./MartianRobotsApp/MartianRobotsApp/MartianRobotsApp.csproj --disable-parallel -c release --no-restore
+#dotnet restore ./MartianRobotsApp/MartianRobotsApp/MartianRobotsApp.csproj --disable-parallel
+#dotnet build ./MartianRobotsApp/MartianRobotsApp/MartianRobotsApp.csproj --disable-parallel -c release --no-restore
+
+echo ""
+echo "The process has finished."
+read -s -n 1 -p "Press any key to continue..."
+echo ""
---
+++ WindowsSetup.ps1
@@ -0,0 +1,13 @@
+docker network create codechallenge-network
+
+docker run --name CodeChallengeDB --network codechallenge-network -p 1433:1433 -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Cod3.Challeng3' -d mcr.microsoft.com/mssql/server:2022-latest
+
+docker build -t code-challenge:v1 ./API
+
+docker run --name CodeChallengeAPI --network codechallenge-network -p 5005:5005 -e 'ASPNETCORE_URLS=http://*:5005' -d code-challenge:v1
+
+dotnet restore ./MartianRobotsApp/MartianRobotsApp/MartianRobotsApp.csproj --disable-parallel
+dotnet build ./MartianRobotsApp/MartianRobotsApp/MartianRobotsApp.csproj --disable-parallel -c release --no-restore
+
+Write-Host "The process has finished."
+pause
